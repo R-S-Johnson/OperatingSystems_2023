2e206c72dce415706ed4859962f66349342be5a6
Riley Johnson <riley.scott.johnson@gmail.com>
2023-01-31 19:40:05 -0500

    list.h compile without error

--- HW0/list.h
+++ HW0/list.h
@@ -4,39 +4,65 @@
 #ifndef LIST_H
 #define LIST_H
 
-static inline struct linked_list *
-ll_create(void)
-{
-	return NULL;
+struct node {
+    int value;
+    struct node* next;
+};
+
+struct linked_list {
+    int size;
+    struct node* head;
+};
+
+static inline struct linked_list * ll_create(void) {
+    struct linked_list* ll = (struct linked_list*)malloc(sizeof(struct linked_list));
+    ll->size = 0;
+	return ll;
 }
 
-static inline int
-ll_destroy(struct linked_list *ll)
-{
-
+static inline int ll_destroy(struct linked_list *ll) {
+    if (ll->size == 0) {
+        free(ll);
+        return 1;
+    }
+    return 0;
 }
 
-static inline void
-ll_add(struct linked_list *ll, int value)
-{
-
+static inline void ll_add(struct linked_list *ll, int value) {
+    struct node *toEnter = (struct node*)malloc(sizeof(struct node));
+    toEnter->value = value;
+    struct node *tmp = ll->head;
+    ll->head = toEnter;
+    toEnter->next = tmp;
+    free(tmp);
+    ll->size++;
 }
 
-static inline int
-ll_length(struct linked_list *ll)
-{
-	return 0;
+static inline int ll_length(struct linked_list *ll) {
+	return (ll->size);
 }
 
-static inline bool
-ll_remove_first(struct linked_list *ll)
-{
-	return NULL;
+static inline int ll_remove_first(struct linked_list *ll) {
+    if (ll->size == 0) {
+        return 0;
+    }
+    struct node *tmp = ll->head;
+    ll->head = tmp->next;
+    free(tmp);
+	return 1;
 }
 
-static inline int
-ll_contains(struct linked_list *ll, int value)
-{
+static inline int ll_contains(struct linked_list *ll, int value) {
+    if (ll->size == 0) {
+        return 0;
+    }
+    struct node *cur = ll->head;
+    for (int i = 0; i < ll->size; i++) {
+        if (cur->value == value) {
+            return (i+1);
+        }
+        cur = cur->next;
+    }
 	return 0;
 }
 
